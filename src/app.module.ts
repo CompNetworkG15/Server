6f91d6c598649b44ffdbaab211b829535e56cd87
kanin <kanin49144@gmail.com>
2023-04-05 19:15:21 +0700

    remove not used file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatGateway } from './chat.gateway';
 import { ClientModule } from './modules/client/client.module';
 import { ChatgroupModule } from './modules/chat-group/chat-group.module';
 import { ChatModule } from './modules/chat/chat.module';
